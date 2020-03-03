a22aec08e024bf6ace5586d7663066f5c2cbb6a2
Evans Chen <evansschen@gmail.com>
2020-03-03 10:38:10 -0600

    Gives health bar some functionality

---
+++ Assets/Scripts/UI/CombatHealthBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatHealthBar : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    public int HPPerBar;
+    // width of fill bar at full health
+    public float fillMax;
+    // width of fill bar at one health
+    public float fillMin;
+
+    public RectTransform frame;
+    public RectTransform fill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMaxHealth(18);
+        SetHealth(13);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetHealth(int health)
+    {
+        float oneBarWidth = (fillMax - fillMin) / (maxHP - HPPerBar);
+        fill.sizeDelta = new Vector2(fillMin + oneBarWidth * (health - HPPerBar), fill.sizeDelta.y);
+        currentHP = health;
+
+    }
+    public void SetMaxHealth(int maxHealth)
+    {
+        float oneBarWidth = (fillMax - fillMin) / (maxHP - HPPerBar);
+        fillMax += oneBarWidth * (maxHealth - maxHP);
+        frame.sizeDelta = new Vector2(frame.sizeDelta.x + oneBarWidth * (maxHealth - maxHP), frame.sizeDelta.y);
+        maxHP = maxHealth;
+        
+    }
+}
