8c957ae540e8a2b3c02cab9cf501dbc0490e69ec
Evans Chen <evansschen@gmail.com>
2020-03-03 18:25:21 -0600

    Adds combat trait/weakness enums

---
+++ Assets/Scripts/CombatTraits.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// stores all traits and counters for this run
+public class CombatTraits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum DamageType
+{
+    Heat, Cold, Electric, Acid, Light, Psychic, Holy, Unholy,
+    Piercing, Slashing, Bludgeoning
+}
+
+public enum DamageDeliveryType
+{
+    Fire, Water, Earth, Weapon, Material, Magic
+}
+
+public enum WeaponType
+{
+    Sword, Spear, Dagger
+}
+
+public enum MaterialType
+{
+    Iron, Steel, Brass, Bronze, Copper, Gold, Silver, Wood
+}
+
+public enum BiomeType
+{
+    Mountain, Plains, Beach, Swamp
+}
+
+public enum MoonType
+{
+    Full, New, Crescent, Gibbous, Eclipse
+}
+
+public enum WeatherType
+{
+    Sun, Cloud, Rain, Storm
+}
